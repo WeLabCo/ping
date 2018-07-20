f3c66f1d3153db7aaf4978d7cfac3ae1974d59cf
Ken Tong <canton1440@gmail.com>
2018-07-20 14:04:00 +0800

    skip logging `ping` unable to write unit test without adding an extra gem

--- app/controllers/ping/application_controller.rb
+++ app/controllers/ping/application_controller.rb
@@ -1,7 +1,26 @@
 module Ping
   class ApplicationController < ActionController::API
+    FORCE_LOGGING = !!ENV.fetch('FORCE_LOG_PING', false)
+
     def ping
       render plain: "PONG"
     end
+
+    def process(action, *args)
+      if force_log_ping?
+        super
+      else
+        logger.silence do
+          super
+        end
+      end
+    end
+
+    private
+
+    def force_log_ping?
+      FORCE_LOGGING
+    end
+
   end
 end
