4872ad6607899414e4a081be13e2718c49682e4a
Ken Tong <canton1440@gmail.com>
2018-11-12 10:38:52 +0800

    [Tech-only] silence log (#1)
    * skip logging `ping`
    unable to write unit test without adding an extra gem
    
    * adding unit tests
    
    * feedback update
    
    * dump version

--- app/controllers/ping/application_controller.rb
+++ app/controllers/ping/application_controller.rb
@@ -1,7 +1,24 @@
 module Ping
   class ApplicationController < ActionController::API
+    FORCE_LOGGING = !!ENV.fetch('FORCE_LOG_PING', false)
+
     def ping
       render plain: "PONG"
     end
+
+    def process(action, *args)
+      if self.class.force_log_ping?
+        super
+      else
+        logger.silence do
+          super
+        end
+      end
+    end
+
+    def self.force_log_ping?
+      FORCE_LOGGING
+    end
+
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
 require File.expand_path("../../test/dummy/config/environment.rb", __FILE__)
 require "rails/test_help"
+require 'minitest/mock'
 Minitest.backtrace_filter = Minitest::BacktraceFilter.new
